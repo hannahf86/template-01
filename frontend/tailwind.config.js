7ae15f756206909ba6a7abe4b4bb9d6b57ab083c
hannahf86 <hannahfeehan.design@gmail.com>
2024-02-27 13:26:54 +0000

    theme updated

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,9 +12,30 @@ export default {
         'color-theme-01-hover': '#edf235',
       },
       borderColor: {
-        'color-theme01': '#edf235', 
+        'color-theme-01': '#edf235', 
+      },
+      textColor: {
+        'color-theme-01': '#edf235', 
       }
     },
   },
+  safelist: [
+    {
+      pattern: /text-color-(theme-01)/
+    },
+    {
+      pattern: /bg-color-(theme-01)/
+    },
+    {
+      pattern: /bg-color-card-(theme-01)/
+    },
+    {
+      pattern: /bg-color-card-(theme-01)/,
+      variants: ['hover']
+    },
+    {
+      pattern: /border-color-(theme-01)/
+    },
+  ],
   plugins: [],
 }
